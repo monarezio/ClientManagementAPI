d396f28c8d857442b757668d1330b556daad2c3a
Samuel Kodytek <samuel.kodytek@guided.cz>
2022-02-07 22:55:05 +0100

    aaa

--- ClientManagementAPI/Program.cs
+++ ClientManagementAPI/Program.cs
@@ -14,6 +14,7 @@ builder.Services.AddCors(o => o.AddPolicy("MyPolicy", builder =>
 }));
 
 var app = builder.Build();
+app.UseCors("MyPolicy");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
